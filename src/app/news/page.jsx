1a2b7ab64c8b1602cad0c45f30e843809fc2e1c7
AbuDzarAlGhifari <rasduba@gmail.com>
2024-12-16 10:58:57 +0700

    update: HomePage

--- src/app/news/page.jsx
+++ src/app/news/page.jsx
@@ -1,10 +1,12 @@
-import NewsList from '../../features/news/NewsList';
+import NewsList from '@/features/news/NewsList';
 
-export default function NewsPage() {
+const NewsPage = () => {
   return (
     <div className="container p-4 mx-auto">
       <h1 className="mb-4 text-3xl font-bold">Latest News</h1>
       <NewsList query="" params={{ country: 'us', page: 1, pageSize: 10 }} />
     </div>
   );
-}
+};
+
+export default NewsPage;
--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,76 +1,41 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
-import Link from 'next/link';
-import axios from 'axios';
-import { HiSearchCircle } from 'react-icons/hi';
-import { IoMoon, IoSearch, IoSunny } from 'react-icons/io5';
 import { logo_web } from '@/assets/index';
+import useCategories from '@/hooks/useCategories';
 import Image from 'next/image';
+import Link from 'next/link';
+import { useState } from 'react';
+import { HiSearchCircle } from 'react-icons/hi';
+import { IoSearch } from 'react-icons/io5';
 
 const Navbar = () => {
-  const [categories, setCategories] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
   const [isSearchActive, setIsSearchActive] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const [themeMode, setThemeMode] = useState('light');
-
-  const API_KEY = process.env.NEXT_PUBLIC_NEWS_API_KEY;
-
-  const fetchCategories = async () => {
-    try {
-      setLoading(true);
-      const response = await axios.get('https://newsapi.org/v2/sources', {
-        params: {
-          apiKey: API_KEY,
-        },
-      });
-
-      const uniqueCategories = [
-        ...new Set(response.data.sources.map((source) => source.category)),
-      ];
-      setCategories(uniqueCategories);
-    } catch (error) {
-      console.error('Error fetching categories:', error);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    fetchCategories();
-  }, []);
+  const { categories, loading } = useCategories();
 
   const handleSearch = (e) => {
     e.preventDefault();
     console.log('Searching for:', searchTerm);
-
     setIsSearchActive(false);
-  };
-
-  // Apply theme to the document
-  useEffect(() => {
-    document.documentElement.classList.toggle('dark', themeMode === 'dark');
-  }, [themeMode]);
-
-  // Toggle the theme
-  const toggleTheme = () => {
-    setThemeMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
   };
 
   return (
     <nav className="p-4 bg-white border-b border-gray-600">
-      <div className="container flex items-center justify-between pb-4 mx-auto ">
+      <div className="container flex items-center justify-between max-w-6xl pb-4 mx-auto">
         {/* Logo */}
         <Link href="/">
           <Image src={logo_web} alt="logo" className="w-24" />
         </Link>
 
-        {/* Search */}
+        {/* Search and Theme Toggle */}
         <div className="flex items-center gap-2 ">
-          <div className="flex gap-2">
-            <h1>News</h1>
-            <h1>About</h1>
+          <div className="flex gap-2 ">
+            <h1 className="text-sm font-medium text-gray-600 capitalize hover:text-black">
+              News
+            </h1>
+            <h1 className="text-sm font-medium text-gray-600 capitalize hover:text-black">
+              category
+            </h1>
           </div>
           <div>
             {isSearchActive ? (
@@ -81,7 +46,7 @@ const Navbar = () => {
                   placeholder="Search..."
                   value={searchTerm}
                   onChange={(e) => setSearchTerm(e.target.value)}
-                  className="px-2 border border-gray-300 rounded-l focus:outline-none focus:ring-2 focus:ring-black"
+                  className="px-2 border border-gray-300 rounded-l max-w-28 focus:outline-none focus:ring-2 focus:ring-black"
                 />
                 <button
                   type="submit"
@@ -94,18 +59,10 @@ const Navbar = () => {
               // Search Icon (initial state)
               <HiSearchCircle
                 className="text-gray-500 cursor-pointer size-6 hover:text-gray-800"
-                onClick={() => setIsSearchActive(true)} // Switch to the form
+                onClick={() => setIsSearchActive(true)}
               />
             )}
           </div>
-
-          <button onClick={toggleTheme}>
-            {themeMode === 'light' ? (
-              <IoMoon className="text-gray-900 p-0.5 rounded-full bg-gray-400 size-5" />
-            ) : (
-              <IoSunny className="p-0.5 text-yellow-400 bg-gray-800 rounded-full size-5" />
-            )}
-          </button>
         </div>
       </div>
 
